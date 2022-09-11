5b05f680a45e0af4ea6b7f20dd5f6df2f65627c2
drsquared <109891293+drsquared@users.noreply.github.com>
2022-09-11 12:25:27 +1200

    Add files via upload

---
+++ clearCheckbox.R
@@ -0,0 +1,22 @@
+library(shiny)
+ui <- fluidPage(
+  wellPanel(checkboxGroupInput("dbr", selected = NULL,
+                               h4("Select Data Breach Rating"),
+                               c("Low" = "Low",
+                                 "Medium" = "Medium",
+                                 "High" = "High",
+                                 "Critical" = "Critical")),
+            actionButton("reset_artifact_entry", label = "Reset")
+  )
+)
+
+server <- function(input, output, session) {
+  # Rest button for artifact entry page
+  observeEvent(input$reset_artifact_entry, {
+    updateCheckboxGroupInput(session, "dbr", choices = c(
+      "Low" = "Low",
+      "Medium" = "Medium",
+      "High" = "High",
+      "Critical" = "Critical"), selected = NULL)})
+}
+shinyApp(ui, server)
